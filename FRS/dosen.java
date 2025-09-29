2be728485e84fec93d578cf6a4e03f9ff131662d
Nashwa Aulia <98628350+nanaaapid@users.noreply.github.com>
2025-09-29 13:03:10 +0700

    Create dosen.java

---
+++ FRS/dosen.java
@@ -0,0 +1,21 @@
+public class Dosen {
+    private String nama;
+    private String nip;
+
+    public Dosen(String nama, String nip) {
+        this.nama = nama;
+        this.nip = nip;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Dosen: " + nama + " | NIP: " + nip);
+    }
+}
