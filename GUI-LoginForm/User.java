dadfd5cf821d67e099636f1729477b7a7328135a
Nashwa Aulia <98628350+nanaaapid@users.noreply.github.com>
2025-11-24 23:22:01 +0700

    Create User.java

---
+++ GUI-LoginForm/User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String username;
+    private String password;
+
+    public User(String u, String p) {
+        this.username = u;
+        this.password = p;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
