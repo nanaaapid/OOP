058f9ec4338ffe97afb61d9328c40a8cc45d1d15
Nashwa Aulia <98628350+nanaaapid@users.noreply.github.com>
2025-11-24 23:23:54 +0700

    Create MainApp.java

---
+++ GUI-ImageViewer/MainApp.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ViewerFrame f = new ViewerFrame();
+                f.setVisible(true);
+            }
+        });
+    }
+}
