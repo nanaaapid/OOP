3cc555c0d3d7093ecd5c769e80cdeeb6cc1f6e7c
Nashwa Aulia <98628350+nanaaapid@users.noreply.github.com>
2025-11-25 11:28:03 +0700

    Create GameFrame.java

---
+++ GamePong/GameFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
